da5f61e73dcefa83d227e7e8287460266cd754eb
moh1415 <moh14151415@gmail.com>
2020-08-30 21:44:21 +0300

    Updates

---
+++ precache-manifest.45a47b112260286688617f98b89dcbc7.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c016971b7f2b96116ea669842d54f19",
+    "url": "/music_app/index.html"
+  },
+  {
+    "revision": "b239038569f348d1c52b",
+    "url": "/music_app/static/css/2.e1d0dfee.chunk.css"
+  },
+  {
+    "revision": "77d0b0c4f85693d8d8e5",
+    "url": "/music_app/static/css/3.6152f2f4.chunk.css"
+  },
+  {
+    "revision": "76feea691923cfd45c48",
+    "url": "/music_app/static/css/main.e953c9e1.chunk.css"
+  },
+  {
+    "revision": "696ddf57311dcec4f359",
+    "url": "/music_app/static/js/10.24e88115.chunk.js"
+  },
+  {
+    "revision": "0eb7dbeec726c92d4986",
+    "url": "/music_app/static/js/11.482badab.chunk.js"
+  },
+  {
+    "revision": "b239038569f348d1c52b",
+    "url": "/music_app/static/js/2.9f447a32.chunk.js"
+  },
+  {
+    "revision": "77d0b0c4f85693d8d8e5",
+    "url": "/music_app/static/js/3.53251065.chunk.js"
+  },
+  {
+    "revision": "e6a013ec84071d6120ba",
+    "url": "/music_app/static/js/4.336c8d77.chunk.js"
+  },
+  {
+    "revision": "a6d31b048adc0ce4fc02",
+    "url": "/music_app/static/js/5.69ac8bab.chunk.js"
+  },
+  {
+    "revision": "ef949dcc44d445f61790",
+    "url": "/music_app/static/js/6.f955a994.chunk.js"
+  },
+  {
+    "revision": "5ad00ff24d660e7b3fa8",
+    "url": "/music_app/static/js/7.08a29abd.chunk.js"
+  },
+  {
+    "revision": "7be2a85a504bdcda3618",
+    "url": "/music_app/static/js/8.52942a3f.chunk.js"
+  },
+  {
+    "revision": "60aaf097a62669252c73",
+    "url": "/music_app/static/js/9.ec00bc99.chunk.js"
+  },
+  {
+    "revision": "76feea691923cfd45c48",
+    "url": "/music_app/static/js/main.6d4b6748.chunk.js"
+  },
+  {
+    "revision": "0f8ce70749f400b5e44c",
+    "url": "/music_app/static/js/runtime~main.8ab78f7e.js"
+  },
+  {
+    "revision": "ffcb10fda07458ebaac2b7f7bf2021f5",
+    "url": "/music_app/static/media/CompletedTick.ffcb10fd.svg"
+  },
+  {
+    "revision": "e927ae19d591028d5fc5a9363fa13e7a",
+    "url": "/music_app/static/media/DownloadIcon.e927ae19.svg"
+  },
+  {
+    "revision": "88d36f16434a80e7c318742f0d68408d",
+    "url": "/music_app/static/media/Paytm.88d36f16.svg"
+  },
+  {
+    "revision": "c96ca8939fd97b056cc9918f83b5b7e1",
+    "url": "/music_app/static/media/dottedCircle.c96ca893.svg"
+  },
+  {
+    "revision": "8fb2bc7a29a4264bdcdfb156b065fea6",
+    "url": "/music_app/static/media/google.8fb2bc7a.svg"
+  },
+  {
+    "revision": "bfe0ee1918a0e1c38729a97968a0eca2",
+    "url": "/music_app/static/media/moon-solid.bfe0ee19.svg"
+  },
+  {
+    "revision": "2a54ea042c1dc1d17e64daeeccca791d",
+    "url": "/music_app/static/media/music-bg.2a54ea04.svg"
+  },
+  {
+    "revision": "201e4d0f513a5bdf7537f8a83f15d125",
+    "url": "/music_app/static/media/paytmQR.201e4d0f.jpg"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/music_app/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/music_app/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/music_app/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/music_app/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/music_app/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/music_app/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/music_app/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/music_app/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/music_app/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/music_app/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/music_app/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/music_app/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/music_app/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/music_app/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/music_app/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/music_app/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/music_app/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/music_app/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/music_app/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/music_app/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/music_app/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/music_app/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/music_app/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/music_app/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "f4e2a7700c9491144433d9029e262f6a",
+    "url": "/music_app/static/media/sun-solid.f4e2a770.svg"
+  }
+]);
